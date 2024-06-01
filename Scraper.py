658641ca431301f8e524c7bc18ca7d5cd5cbca07
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-05-31 21:31:08 -0400

    added scrape protocol specifier for different site tags and classnames

--- Scraper.py
+++ Scraper.py
@@ -15,9 +15,10 @@ class JobPosting:
 
 class Company:
 
-    def __init__(self, site_link):
+    def __init__(self, site_link, scrape_protocol):
         self.previous_postings = []  # Ensure only the last 20 postings are stored
         self.site_link = site_link
+        self.scrape_protocol = scrape_protocol
 
 class Driver:
 
@@ -28,8 +29,8 @@ class Driver:
 # Example usage:
 
 if __name__ == "__main__":
-    google = Company(site_link="https://www.google.com/about/careers/applications/jobs/results?employment_type=INTERN&location=United%20States&target_level=INTERN_AND_APPRENTICE")
-    amazon = Company(site_link="https://www.amazon.jobs/content/en/career-programs/student-programs?country%5B%5D=US")
-    capitalOne = Company(site_link="https://capitalone.wd1.myworkdayjobs.com/Capital_One?workerSubType=a12c70bf789e10572aab83c4780919ad")
+    google = Company(site_link="https://www.google.com/about/careers/applications/jobs/results?employment_type=INTERN&location=United%20States&target_level=INTERN_AND_APPRENTICE", scrape_protocol=('div',"sMn82b"))
+    amazon = Company(site_link="https://www.amazon.jobs/content/en/career-programs/student-programs?country%5B%5D=US", scrape_protocol=('div','job-card-module_root__QYXVA'))
+    capitalOne = Company(site_link="https://capitalone.wd1.myworkdayjobs.com/Capital_One?workerSubType=a12c70bf789e10572aab83c4780919ad", scrape_protocol=('li','css-1q2dra3'))
 
  
